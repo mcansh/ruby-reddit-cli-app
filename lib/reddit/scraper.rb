00b57e8a6129df3042762eed2855b7aaa9f8e6aa
Logan McAnsh <logan@mcan.sh>
2017-12-04 14:16:19 -0500

    scrape r/ruby

--- lib/reddit/scraper.rb
+++ lib/reddit/scraper.rb
@@ -1,2 +1,17 @@
 class Reddit::Scraper
+  attr_accessor :title, :author, :timestamp, :comments, :upvotes
+  def self.scrape
+    doc = Nokogiri::HTML(open("https://www.reddit.com/r/ruby", 'User-Agent' => 'ruby-reddit'))
+    posts = []
+    postsList = doc.css('#siteTable > div.thing.link').first(10)
+    postsList.each do |post|
+      title = post.css('.title a').text.strip
+      author = post.css('.author').text.strip
+      timestamp = post.css('.live-timestamp').text.strip
+      comments = post.css('.comments').text.strip
+      upvotes = post.css('.score.unvoted').text.strip
+      posts << {title: title, author: author, timestamp: timestamp, comments: comments, upvotes: upvotes}
+    end
+    posts
+  end
 end
