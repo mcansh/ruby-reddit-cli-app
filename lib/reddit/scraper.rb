d4cf853d79f86c9f063f201abb55f3338e77a50c
Logan McAnsh <logan@mcan.sh>
2017-12-04 22:32:34 -0500

    update hash to hold url, and source if any

--- lib/reddit/scraper.rb
+++ lib/reddit/scraper.rb
@@ -1,5 +1,5 @@
 class Reddit::Scraper
-  attr_accessor :title, :author, :timestamp, :comments, :upvotes
+  attr_accessor :title, :author, :timestamp, :comments, :upvotes, :url, :text
   def self.scrape
     doc = Nokogiri::HTML(open("https://www.reddit.com/r/ruby", 'User-Agent' => 'ruby-reddit'))
     posts = []
@@ -8,9 +8,20 @@ class Reddit::Scraper
       title = post.css('.title a.title').text.strip
       author = post.css('.author').text.strip
       timestamp = post.css('.live-timestamp').text.strip
-      comments = post.css('.comments').text.strip
-      upvotes = post.css('.score.unvoted').text.strip
-      posts << {title: title, author: author, timestamp: timestamp, comments: comments, upvotes: upvotes}
+      comments = post.attr('data-comments-count')
+      upvotes = post.attr('data-score')
+      url = post.css('a.comments').attr('href')
+      text = post.css('.viewSource textarea').text
+      newPost = {
+        title: title,
+        author: author,
+        timestamp: timestamp,
+        comments: comments,
+        upvotes: upvotes,
+        url: url,
+        text: text
+      }
+      posts << newPost
     end
     posts
   end
